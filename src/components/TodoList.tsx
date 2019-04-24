268ecc625240ba5022999d577a216a3d71787ec7
Robert Donigian <donigianrp@gmail.com>
2019-04-24 07:10:31 -0400

    removed displayed, setDisplayed useState

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,28 +1,16 @@
-import React, { FunctionComponent, useState, useEffect } from "react";
+import React, { FunctionComponent, useState } from "react";
 import "../App.css";
 import { Todo } from "../react-app-env";
 import TodoItem from "./TodoItem";
 
 interface Props {
   todos: Todo[];
-  displayed: Todo[];
   setTodos: (todos: Todo[]) => void;
-  setDisplayed: (todos: Todo[]) => void;
 }
 
 const InputTodo: FunctionComponent<Props> = props => {
-  const { todos, displayed, setDisplayed, setTodos } = props;
+  const { todos, setTodos } = props;
   const [selected, setSelected] = useState<string>("");
-
-  useEffect(() => {
-    if (selected === "Active") {
-      setDisplayed(todos.filter(todo => todo.completed === false));
-    } else if (selected === "Completed") {
-      setDisplayed(todos.filter(todo => todo.completed === true));
-    } else {
-      setDisplayed(todos);
-    }
-  }, [todos]);
 
   const itemsLeft = () => {
     const uncompleted = todos.filter(todo => todo.completed === false);
@@ -31,21 +19,24 @@ const InputTodo: FunctionComponent<Props> = props => {
       ? `${uncompleted.length} item left`
       : `${uncompleted.length} items left`;
   };
+
   const handleOption = (option: string) => {
-    // selected === option ? setSelected("") :
     setSelected(option);
-    if (option === "Active") {
-      setDisplayed(todos.filter(todo => todo.completed === false));
-    } else if (option === "Completed") {
-      setDisplayed(todos.filter(todo => todo.completed === true));
+  };
+
+  const handleDisplayed = () => {
+    if (selected === "Active") {
+      return todos.filter(todo => todo.completed === false);
+    } else if (selected === "Completed") {
+      return todos.filter(todo => todo.completed === true);
     } else {
-      setDisplayed(todos);
+      return todos;
     }
   };
 
   return (
     <>
-      {displayed.map(todo => (
+      {handleDisplayed().map(todo => (
         <TodoItem key={todo.id} todo={todo} setTodos={setTodos} todos={todos} />
       ))}
       <div className="todoFooter">
